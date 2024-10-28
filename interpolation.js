a4ff131deefeddc310962e1cb89efbb32f946a94
KMaleesha <maleeshakumarasinghe@gmail.com>
2024-10-28 08:52:11 +0530

    backtick key

---
+++ interpolation.js
@@ -0,0 +1,5 @@
+const name = "Maleesha";
+const age  = 25;
+
+console.log("my name is "+name+ ". "+"I am " +age+" years old.");
+console.log(`my name is ${name}. I am ${age} years old`);
---
+++ tamplate_litterel.js
@@ -0,0 +1,13 @@
+// template literal with backtick key
+const text1 = "My Name is Maleesha";
+const text4 = "My \nName \nis \tMaleesha";
+console.log(text1);
+console.log(text4);
+
+const text2 = `My name is Tharuprabha`;
+const text3 = `My 
+name 
+is 
+Tharuprabha`;
+console.log(text2);
+console.log(text3);
